b26020f316ae8a4b609f55db2a4411042ad2b1ed
The Learner <samarkant1@yahoo.com>
2021-08-27 08:39:12 +0530

    Add files via upload

---
+++ dags/template_dag.py
@@ -0,0 +1,57 @@
+import sys
+import airflow
+from airflow import DAG, macros
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+# from airflow.operators.postgres_operator import PostgresOperator
+from datetime import datetime, timedelta
+
+# Would be cleaner to add the path to the PYTHONPATH variable
+sys.path.insert(1, '/mnt/d/airflow/dags/scripts')
+
+from process_logs import process_logs_func
+
+TEMPLATED_LOG_DIR = """{{ var.value.source_path }}/data/{{ macros.ds_format(ts_nodash, "%Y%m%dT%H%M%S", "%Y-%m-%d-%H-%M") }}/"""
+
+default_args = {
+            "owner": "Airflow",
+            "start_date": airflow.utils.dates.days_ago(1),
+            "depends_on_past": False,
+            "email_on_failure": False,
+            "email_on_retry": False,
+            "email": "youremail@host.com",
+            "retries": 1
+        }
+
+with DAG(dag_id="a_template_dag", schedule_interval="@daily", default_args=default_args) as dag:
+
+    t0 = BashOperator(
+            task_id="t0",
+            # bash_command = "echo {{ ds }}"
+            # bash_command = "echo {{ var.value.TEST_VARIABLE }}"
+            # bash_command="echo {{ macros.ds_format(ts_nodash, '%Y%m%dT%H%M%S', '%Y-%m-%d-%H-%M') }}"
+            bash_command="echo {{ ds }};echo {{ var.value.TEST_VARIABLE }}; echo {{ macros.ds_format(ts_nodash, '%Y%m%dT%H%M%S', '%Y-%m-%d-%H-%M') }}"
+            )
+
+    # template_ext=['.sh','.bash'] for the BashOperator means that template placeholders can be included in the script files with these extensions
+    t1 = BashOperator(
+            task_id="generate_new_logs",
+            bash_command="./scripts/generate_new_logs.sh",
+            params={'filename': 'log.csv'})
+
+    t2 = BashOperator(
+           task_id="logs_exist",
+           bash_command="test -f " + TEMPLATED_LOG_DIR + "log.csv",
+           )
+    # provide_context is default True - so can be skipped
+    # 
+    t3 = PythonOperator(
+           task_id="process_logs",
+           python_callable=process_logs_func,
+           provide_context=True,
+           templates_dict = {'log_dir': TEMPLATED_LOG_DIR},
+           params={'filename': 'log.csv'}
+           )
+
+    #t0 >> t1 >> t2 >> t3
+    t0 >> t1 >> t2 >> t3
